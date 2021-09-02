1d3000ed836e5561fece723b6b31e7c88e00935a
Gromhak <aurelien.malve@gmail.com>
2021-09-02 10:08:35 +0200

    install djangorestframework

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls'))
 ]
