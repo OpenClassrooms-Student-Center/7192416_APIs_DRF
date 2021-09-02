a78982b765f964d74505fca31f4230d0da402b0f
Gromhak <aurelien.malve@gmail.com>
2021-09-02 11:55:41 +0200

    Endpoint category use Viewset

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,16 @@
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
 
-from shop.views import CategoryView, ProductView
+from shop.views import CategoryViewset, ProductView
 
+
+router = routers.SimpleRouter()
+router.register('category', CategoryViewset, basename='category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryView.as_view()),
     path('api/product/', ProductView.as_view()),
+    path('api/', include(router.urls))
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,17 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.response import Response
 
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
 
 
-class CategoryView(APIView):
+class CategoryViewset(ReadOnlyModelViewSet):
 
-    def get(self, *args, **kwargs):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 
 class ProductView(APIView):
