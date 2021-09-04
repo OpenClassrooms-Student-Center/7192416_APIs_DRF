1addd3bae7100025c0d473c1c71c1bf52c96367f
Gromhak <aurelien.malve@gmail.com>
2021-09-04 10:26:58 +0200

    Install and configure simple-jwt

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'shop',
 ]
 
@@ -128,5 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,8 @@ from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from shop.views import CategoryViewset, ProductViewset, ArticleViewset,\
     AdminCategoryViewset, AdminArticleViewset
 
@@ -17,5 +19,7 @@ router.register('admin/article', AdminArticleViewset, basename='admin-article')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='obtain_tokens'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('api/', include(router.urls))
 ]
